b23bebb7e7fd8ef85d39c2cb6de2d0aba56ee873
nafismahamudshahin <nafismahamudshahin@gmail.com>
2026-09-24 11:49:12 +0600

    add my plan page

---
+++ src/app/my-plan/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MyPlanPage = () => {
+    return (
+        <section className='container mx-auto'>
+            Exercise Plan page
+        </section>
+    );
+};
+
+export default MyPlanPage;
